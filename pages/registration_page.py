148a3de94b1ae041e13d8db21d2ef7195dff467d
OvsyannikovIA <kokos.demon.bit@gmail.com>
2024-01-13 19:34:01 +0300

    Refactoring

--- pages/registration_page.py
+++ pages/registration_page.py
@@ -1,112 +1,131 @@
-import os
-
 import allure
 from selene import browser, have
 
-F_NAME = '#firstName'
-L_NAME = '#lastName'
-EMAIL = '#userEmail'
-GENDER_1 = '#gender-radio-1'
-GENDER_2 = '#gender-radio-2'
-GENDER_3 = '#gender-radio-3'
-MOBILE = '#userNumber'
-BIRTH = '#dateOfBirthInput'
-MONTH_BIRTH = 'option:nth-child(7)'
-YEAR_BIRTH = 'option:nth-child(94)'
-DAY_BIRTH = 'div.react-datepicker__day--018'
-SUBJECTS = '#subjectsInput'
-HOBBIES_1 = '[for="hobbies-checkbox-1"]'
-HOBBIES_2 = '[for="hobbies-checkbox-2"]'
-HOBBIES_3 = '[for="hobbies-checkbox-2"]'
-PICTURE = '#uploadPicture'
-C_ADDRESS = '#currentAddress'
-STATE = '#react-select-3-input'
-CITY = '#react-select-4-input'
-SUBMIT = '#submit'
-BODY = ' table > tbody > tr:nth-child(1) > td:nth-child(2)'
+import resource
+from data.user import User
+
+
+# GENDER_1 = '#gender-radio-1'
+# GENDER_2 = '#gender-radio-2'
+# GENDER_3 = '#gender-radio-3'
+# HOBBIES_1 = '[for="hobbies-checkbox-1"]'
+# HOBBIES_2 = '[for="hobbies-checkbox-2"]'
+# HOBBIES_3 = '[for="hobbies-checkbox-2"]'
 
 
 class RegistrationPage():
     def __init__(self):
+        self.first_name = browser.element('#firstName')
+        self.last_name = browser.element('#lastName')
+        self.gender = browser.element('#gender-radio-2')
+        self.email = browser.element('#userEmail')
+        self.mobile = browser.element('#userNumber')
+        self.date_of_birth = browser.element('#dateOfBirthInput')
+        self.month_of_birth = browser.element('.react-datepicker__month-select')
+        self.year_of_birth = browser.element('.react-datepicker__year-select')
+        self.subject = browser.element('#subjectsInput')
+        self.adress = browser.element('#currentAddress')
+        self.hobies = browser.element('[for="hobbies-checkbox-2"]')
+        self.state = browser.element('#react-select-3-input')
+        self.city = browser.element('#react-select-4-input')
+        self.picture = browser.element('#uploadPicture')
+        self.submit = browser.element('#submit')
+        self.completed_registration_form = browser.all(' table > tbody > tr').all('td:nth-child(2)')
         pass
+
     @allure.step('Открытие браузера')
     def open_registration_page(self):
         browser.open('/automation-practice-form')
 
     @allure.step('Заполнение поля "имя"')
     def fill_first_name(self, value):
-        browser.element(F_NAME).type(value)
+        self.first_name.type(value)
         return self
 
     @allure.step('Заполнение поля "фамилия"')
     def fill_last_name(self, value):
-        browser.element(L_NAME).type(value)
+        self.last_name.type(value)
         return self
 
-    @allure.step('Выбор пола')
+    @allure.step('Выбор пола муж/жен')
     def choice_gender(self):
-        browser.element(GENDER_2).double_click()
+        self.gender.double_click()
         return self
 
     @allure.step('Заполнение поля "email"')
     def fill_email(self, value):
-        browser.element(EMAIL).type(value)
+        self.email.type(value)
         return self
 
     @allure.step('Заполнение поля "мобильный номер"')
     def fill_user_number(self, value):
-        browser.element(MOBILE).type(value)
+        self.mobile.type(value)
         return self
 
     @allure.step('Выбор даты рождения')
-    def fill_date_of_birth(self):
-        browser.element(BIRTH).click()
-        browser.element(MONTH_BIRTH).click()
-        browser.element(YEAR_BIRTH).click()
-        browser.element(DAY_BIRTH).click()
+    def fill_date_of_birth(self, year, month, day):
+        self.date_of_birth.click()
+        self.month_of_birth.type(month)
+        self.year_of_birth.type(year)
+        browser.element(
+            f'.react-datepicker__day--0{day}:not(.react-datepicker__day--outside-month)'
+        ).click()
         return self
 
     @allure.step('Заполнение поля "предмет"')
     def fill_subjects(self, value):
-        browser.element(SUBJECTS).type(value).press_enter()
+        self.subject.type(value).press_enter()
         return self
 
     @allure.step('Заполнение поля "адрес"')
     def fill_current_address(self, value):
-        browser.element(C_ADDRESS).type(value)
+        self.adress.type(value)
         return self
 
     @allure.step('Выбор хобби')
     def choice_hobies(self):
-        browser.element(HOBBIES_2).click()
+        self.hobies.click()
         return self
 
     @allure.step('Выбор государства и города')
     def fill_state(self, state, city):
-        browser.element(STATE).type(state).press_enter()
-        browser.element(CITY).type(city).press_enter()
+        self.state.type(state).press_enter()
+        self.city.type(city).press_enter()
         return self
 
     @allure.step('Загрузка изображения')
-    def upload_picture(self):
-        browser.element('#uploadPicture').send_keys(os.path.abspath('../resources/picture.png'))
-        return self
+    def upload_picture(self, value):
+        return self.picture.set_value(resource.path(value))
 
     @allure.step('Проверка формы регистрации пользователя')
-    def should_registered_user_with(self):
-        browser.all(' table > tbody > tr').all('td:nth-child(2)').should(
-            have.exact_texts('Мистер Твистер',
-                             'glavniy@ministr.ru',
-                             'Female', '7925002222',
-                             '18 July,1993',
-                             'Maths',
-                             'Reading',
-                             'picture.png',
-                             'Petrovka 38',
-                             'Uttar Pradesh Merrut')
+    def should_registered_form(self, user: User):
+        self.completed_registration_form.should(
+            have.exact_texts(f'{user.f_name} {user.l_name}',
+                             user.email,
+                             user.gender,
+                             user.mobile,
+                             f'{user.day_of_birth} {user.month_of_birth},{user.year_of_birth}',
+                             user.subject,
+                             user.hobbie,
+                             user.picture_name,
+                             user.address,
+                             f'{user.state} {user.city}')
         )
 
     @allure.step('Клик на кнопку зарегистрироваться')
     def submit_btn(self):
-        browser.element(SUBMIT).press_enter()
-        return self
+        return self.submit.press_enter()
+
+    def user_registration(self, user: User):
+        self.fill_first_name(user.f_name)
+        self.fill_last_name(user.l_name)
+        self.fill_email(user.email)
+        self.choice_gender()
+        self.fill_user_number(user.mobile)
+        self.fill_date_of_birth(user.year_of_birth, user.month_of_birth, user.day_of_birth)
+        self.fill_subjects(user.subject)
+        self.fill_current_address(user.address)
+        self.choice_hobies()
+        self.fill_state(user.state, user.city)
+        self.upload_picture(user.picture_name)
+        self.submit_btn()
--- tests/test_demoqa.py
+++ tests/test_demoqa.py
@@ -1,21 +1,24 @@
+from data.user import User
 from pages.registration_page import RegistrationPage
+
+student = User(f_name='Мистер',
+               l_name='Твистер',
+               email='glavniy@ministr.ru',
+               gender='Female',
+               mobile='7925002222',
+               day_of_birth=18,
+               month_of_birth='July',
+               year_of_birth=1993,
+               subject='Maths',
+               hobbie='Reading',
+               picture_name='picture.png',
+               address='Petrovka 38',
+               state='Uttar Pradesh',
+               city='Merrut')
 
 
 def test_fill_form():
     registration = RegistrationPage()
     registration.open_registration_page()
-    (registration
-     .fill_first_name('Мистер')
-     .fill_last_name('Твистер')
-     .fill_email('glavniy@ministr.ru')
-     .choice_gender()
-     .fill_user_number('79250022225')
-     .fill_date_of_birth()
-     .fill_subjects('Maths')
-     .fill_current_address('Petrovka 38')
-     .choice_hobies()
-     .fill_state('Uttar Pradesh', 'Merrut')
-     .upload_picture()
-     .submit_btn()
-     )
-    registration.should_registered_user_with()
+    registration.user_registration(student)
+    registration.should_registered_form(student)
